d253b3e6650c347c371171a866e911763f8a1c76
cma0022 <cma0022@alu.medac.es>
2023-04-25 15:45:44 +0200

    PrimerCommit

---
+++ ejercicio_clase.py
@@ -0,0 +1,17 @@
+# Creamos la clase
+class circulo:
+
+    # Definimos las variables
+    _radio:float
+
+    # Constructor
+    def __init__(self, radio:float) -> None:
+        self._radio = radio
+
+    # Método que no hace nada(por ahora)
+    def area(self):
+        pass
+
+    # Método que no hace nada(por ahora)
+    def circunferencia(self):
+        pass
